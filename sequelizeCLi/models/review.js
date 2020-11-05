4cbfc30b31de71abe16b524b5816fafb9483e28b
Kanilionis <kanilionis@gmail.com>
2020-11-05 10:44:46 -0700

    added user-block and reviews-block handlebars, 5 star-rating, api-routes

---
+++ sequelizeCLi/models/review.js
@@ -0,0 +1,23 @@
+module.exports = function(sequelize, DataTypes) {
+  var Review = sequelize.define("Review", {
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    body: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    privacy: {
+      type: DataTypes.STRING,
+      defaultValue: "Public"
+    }
+  });
+  return Review;
+};
--- sequelizeCLi/models/user.js
+++ sequelizeCLi/models/user.js
@@ -31,3 +31,4 @@ module.exports = function(sequelize, DataTypes) {
 });
   return User;
 };
+
