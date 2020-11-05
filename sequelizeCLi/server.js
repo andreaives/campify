26f9e647d1bb6866299914fecbcaa1021de21963
andreaives <andreasue3@gmail.com>
2020-11-05 11:09:54 -0700

    Updated Serever.js

--- sequelizeCLi/server.js
+++ sequelizeCLi/server.js
@@ -1,10 +1,26 @@
-var express = require("express");
-const db = require("./models");
-
 var app = express();
 var PORT = process.env.PORT || 8080;
 
-db.sequelize.sync().then(function(){  
- app.listen(PORT, function(){    
-  console.log("APP listening on PORT "+ PORT)  
- });});
+// Requiring our models for syncing
+var db = require("./models");
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Static directory
+app.use(express.static("public"));
+
+// Routes
+// =============================================================
+require("./routes/html-routes.js")(app);
+require("./routes/api-routes.js")(app);
+
+
+// Syncing our sequelize models and then starting our Express app
+// =============================================================
+db.sequelize.sync({ force: true }).then(function() {
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
+});
