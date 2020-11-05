2f43e41d23121890fa30c8d3ecf10b2e97c03b67
Kanilionis <kanilionis@gmail.com>
2020-11-05 15:46:38 -0700

    added write-review.js, html outline on reviews-block and user-block handlebars

--- sequelizeCLi/db/seeds.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO users (first_name, last_name, email, password) 
-VALUES ("Katie", "Anilionis", "kanilionis@gmail.com", "password1"), ("Andy", "Ives", "andreasue3@gmail.com", "password2")
