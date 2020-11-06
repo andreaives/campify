ba315462bb2409541651302d7576b0dd992cff01
Kanilionis <kanilionis@gmail.com>
2020-11-06 12:12:58 -0700

    fixing model values

--- sequelizeCLi/models/review.js
+++ sequelizeCLi/models/review.js
@@ -1,33 +1,30 @@
 module.exports = function(sequelize, DataTypes) {
   var Review = sequelize.define("Review", {
     star: {
-      type: DataTypes.INTEGER
-      // ,
-      // allowNull: true,
-      // defaultValue: 3
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 3
     },
     title: {
-      type: DataTypes.TEXT
-      // ,
-      // allowNull: true,
-      // defaultValue: "title",
-      // validate: {
-      //   len: [1, 50]
-      // }
+      type: DataTypes.TEXT,
+      allowNull: false,
+      validate: {
+        len: [1, 50]
+      }
     },
     body: {
-      type: DataTypes.TEXT
-      // ,
-      // allowNull: true,
-      // defaultValue: "body",
-      // validate: {
-      //   len: [1, 255]
-      // }
+      type: DataTypes.TEXT,
+      allowNull: false,
+      validate: {
+        len: [1, 255]
+      }
     },
     privacy: {
-      type: DataTypes.STRING
-      // ,
-      // defaultValue: "Public"
+      type: DataTypes.STRING,
+      defaultValue: "Public"
+    },
+    UserId: {
+      type: DataTypes.INTEGER
     }
   });
   Review.associate = function(models) {
