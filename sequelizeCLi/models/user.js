b74e65a20db2c33c9e4325ed016bd38ba759857b
Travis Cook <travismcook93@gmail.com>
2020-11-06 12:58:51 -0700

    commiting changes to search and login

--- sequelizeCLi/models/user.js
+++ sequelizeCLi/models/user.js
@@ -26,16 +26,16 @@ module.exports = function(sequelize, DataTypes) {
   User.addHook("beforeCreate", function(user) {
     user.password = bcrypt.hashSync(user.password, bcrypt.genSaltSync(10), null)
   })
-};
 
-User.associate = function(models) {
+
+  User.associate = function(models) {
   // We're saying that a Post should belong to an Author
   // A Post can't be created without an Author due to the foreign key constraint
-  User.hasMany(models.Review, {
-      foreignKey: {
-        onDelete: "cascade"
-      }
-    });
-  };
+    User.hasMany(models.Review, {
+        foreignKey: {
+         onDelete: "cascade"
+        }
+      });
+    };
   return User;
-
+};
