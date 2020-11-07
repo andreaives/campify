2040009d19f7c7a1702790324a9a44ab155cf5ab
Erin McGeever <mcgeevee@me.com>
2020-11-07 13:59:38 -0700

    added files

--- sequelizeCLi/public/assets/js/script.js
+++ sequelizeCLi/public/assets/js/script.js
@@ -1,1 +1,23 @@
 
+
+$(document).on('knack-scene-render.Logoutpage', function (event, scene) {
+  let KnackScenes = $(".kn-scenes.kn-section");
+  function redirect (destination = "http:// NEED TO FILL IN") { 
+setTimeout(function(){
+window.location = destination;
+KnackScenes.show(100);
+}, 1000);
+}
+function logoutUser(button = document.createElement("a")) {
+button.className = "navbar-text";
+button.innerHTML = "Click to Log Out";
+document.getElementById("kn-" + scene.key).appendChild(button);
+
+button.addEventListener("click", function() {
+KnackScenes.hide();
+redirect();
+});
+button.click();
+}
+if (Knack.session.user) {logoutUser()} else {redirect()}
+});
