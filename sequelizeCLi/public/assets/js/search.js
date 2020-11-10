181b2219631987af505a6051daef75ec962342f5
Travis Cook <travismcook93@gmail.com>
2020-11-10 11:56:12 -0700

    commiting changes before push

--- sequelizeCLi/public/assets/js/search.js
+++ sequelizeCLi/public/assets/js/search.js
@@ -16,7 +16,7 @@ $(function() { // data from RIDB
           let recAreaSettings = {
             async: true,
             crossDomain: true,
-            url: 'https://cors-anywhere.herokuapp.com/https://ridb.recreation.gov/api/v1/recareas?limit=50&latitude='+userLat+'&longitude='+userLon+'&radius=25&apikey=635989e4-a266-4eac-8549-5bdd1e8435a1', //heroku app is a no-cors fix for the riDB access
+            url: 'https://stormy-cliffs-87695.herokuapp.com/https://ridb.recreation.gov/api/v1/recareas?limit=50&latitude='+userLat+'&longitude='+userLon+'&radius=25&apikey=635989e4-a266-4eac-8549-5bdd1e8435a1', //heroku app is a no-cors fix for the riDB access
             method: "GET",
             contentType: "application/json"
           };
@@ -61,13 +61,20 @@ $(function() { // data from RIDB
                 emailResult.text("Email: " + searchArr[i].email);
                 resultDiv.append(emailResult);
                 var facilityBtn = $("<button>")
-                .addClass("facilitybtn")
+                .addClass("facilityBtn")
                 .text("View Facilities in this Rec Area");
                 facilityBtn.attr('id', "facilityBtn" + i);
-                // facilityBtn.attr('indx', indx);
                 resultDiv.append(facilityBtn)
+                // facilityBtn.attr('indx', indx);
+                var activityBtn = $("<button>")
+                .addClass("activityBtn")
+                .text("View Activities in this Rec Area");
+                activityBtn.attr('id', "activityBtn" + i);
+                resultDiv.append(activityBtn)
+
                 $("#searchResults").append(resultDiv);
               }
+              
               for(a=0;a<50;a++){
                 var facilityListn = document.getElementById('facilityBtn' + a)
                 $(facilityListn).on("click", function(event) {
@@ -79,7 +86,7 @@ $(function() { // data from RIDB
                 let facilitySettings = {
                   async: true,
                   crossDomain: true,
-                  url: 'https://cors-anywhere.herokuapp.com/https://ridb.recreation.gov/api/v1/recareas/'+recIdTarget+'/facilities?limit=50&apikey=635989e4-a266-4eac-8549-5bdd1e8435a1', //heroku app is a no-cors fix for the riDB access
+                  url: 'https://stormy-cliffs-87695.herokuapp.com/https://ridb.recreation.gov/api/v1/recareas/'+recIdTarget+'/facilities?limit=50&apikey=635989e4-a266-4eac-8549-5bdd1e8435a1', //heroku app is a no-cors fix for the riDB access
                   method: "GET",
                   contentType: "application/json"
                 }
@@ -126,11 +133,16 @@ $(function() { // data from RIDB
                 if(searchArr[i].facilityType == "Campground"){
                   var campgroundBtn = $("<button>")
                   .addClass("campgroundbtn")
-                  .text("View Campgrounds for this Facility");
+                  .text("View Campsites for this Campground");
                   campgroundBtn.attr('id', "campgroundBtn" + i);
                   // campgroundBtn.attr('indx', indx);
                   resultDiv.append(campgroundBtn)
                 }
+                var pinButton = $("<button>")
+                pinButton.addClass("pinBtn")
+                pinButton.text("PIN")
+                pinButton.attr("id", "campground" + i)
+                resultDiv.append(pinButton)
                   $("#searchResults").append(resultDiv);
                 }
                 for(a=0;a<50;a++){
@@ -144,20 +156,90 @@ $(function() { // data from RIDB
                   let campgroundSettings = {
                     async: true,
                     crossDomain: true,
-                    url: 'https://cors-anywhere.herokuapp.com/https://ridb.recreation.gov/api/v1/facilities/'+recIdTarget+'/campsites?limit=50&apikey=635989e4-a266-4eac-8549-5bdd1e8435a1', //heroku app is a no-cors fix for the riDB access
+                    url: 'https://stormy-cliffs-87695.herokuapp.com/https://ridb.recreation.gov/api/v1/facilities/'+recIdTarget+'/campsites?limit=50&apikey=635989e4-a266-4eac-8549-5bdd1e8435a1', //heroku app is a no-cors fix for the riDB access
                     method: "GET",
                     contentType: "application/json"
                   }
                   $.ajax(campgroundSettings).done(function(response){
                     var resultArr = response.RECDATA
                     console.log(response)
-                    console.log(resultArr)
+                    resultArr.forEach(function(data) {
+                      var searchItem = {
+                        campsiteID: data.CampsiteID,
+                        campsiteName: data.CampsiteName,
+                        typeofUse: data.TypeOfUse,
+                        campsiteLat: data.CampsiteLatitude,
+                        campsiteLon: data.CampsiteLongitude,
+                        attributes: data.ATTRIBUTES,
+                        permittedEquipment: data.PERMITTEDEQUIPMENT,
+                        parentFacility: data.FacilityID
+                      }
+                      searchArr.push(searchItem)
+                    })
+                    console.log(searchArr)
+                    for(i=0;i<searchArr.length;i++){
+                    var resultDiv = $("<div>");
+                    resultDiv.addClass("searchResult");
+                    resultDiv.attr("facilityId", searchArr[i].facilityID)
+                    var nameResult = $("<div>");
+                    nameResult.addClass("nameResult");
+                    nameResult.text("Campsite Name: " + searchArr[i].campsiteName);
+                    resultDiv.append(nameResult);
+                    var equipmentResult = $("<div>");
+                    equipmentResult.addClass("equipmentResult");
+                    var equipment = (searchArr[i].permittedEquipment)
+                    equipmentDiv = $("<div>")
+                    equipment.forEach(function(data){
+                    equipmentDiv.text("Permitted Equipment: " + data.equipmentName + ", Max Length(ft): " + data.MaxLength);
+                    equipmentResult.append(equipmentDiv);
+                    }) //TODO get equipment rendering nailed down
+                    resultDiv.append(equipmentResult)
+                    var useTypeResult = $("<div>");
+                    useTypeResult.addClass("useTypeResult");
+                    useTypeResult.text("Type of use: " + searchArr[i].typeofUse);
+                    resultDiv.append(useTypeResult);
+                    var coordResult = $("<div>");
+                    coordResult.addClass("coordResult");
+                    coordResult.text("Coordinates: " +"( "+ searchArr[i].campsiteLat + " , " + searchArr[i].campsiteLon+" )");
+                    resultDiv.append(coordResult);
+                    var attributeArr =  searchArr[i].attributes
+                    attributeArr.forEach(function(data){
+                      if(data.AttributeName === "Pets Allowed"){
+                        var petsResult = $("<div>");
+                        petsResult.addClass("petsResult");
+                        petsResult.text("Pets Allowed: " + data.AttributeValue);
+                        resultDiv.append(petsResult);
+                      }
+                    })
+                    var pinButton = $("<button>")
+                    pinButton.addClass("pinBtn")
+                    pinButton.text("PIN")
+                    pinButton.attr("id", "campground" + i)
+                    resultDiv.append(pinButton)
+                    $("#searchResults").append(resultDiv);
+                    }
                   })
+                  $("#searchResults").empty();
                 })
                 }
                 })
                 $("#searchResults").empty();
               })
+                var activityListn = document.getElementById('activityBtn' + a)
+                $(activityListn).on("click", function(event) {
+                  var recareaID = event.target.parentElement.attributes[1].value
+                  let activitySettings = {
+                    async: true,
+                    crossDomain: true,
+                    url: 'https://stormy-cliffs-87695.herokuapp.com/https://ridb.recreation.gov/api/v1/recareas/'+recareaID+'/activities?limit=50&apikey=635989e4-a266-4eac-8549-5bdd1e8435a1', //heroku app is a no-cors fix for the riDB access
+                    method: "GET",
+                    contentType: "application/json"
+                  }
+                  $.ajax(activitySettings).done(function(response){
+                    var resultArr = response.RECDATA
+                    console.log(resultArr)
+                  })
+                })
               }
             })
         $("#searchResults").empty();
